605d48c1481384df3057d70819de698f7203baff
Adel <adeltempname@gmail.com>
2017-11-05 14:14:28 +0100

    additionfeature1

--- main.c
+++ main.c
@@ -5,10 +5,16 @@ int addition (int A, int B)
 {
     return A+B;
 }
+int soustraction (int A, int B)
+{
+    return A-B;
+
+}
 int main()
 {
 
     printf("L'addition de a et b est égale à %d\n", addition(1,4));
+    printf("La soustraction de a et b est égale à %d\n", soustraction(1,4));
 
     return 0;
 }
